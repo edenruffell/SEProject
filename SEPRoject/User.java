13cfa21c997f70f7e7ea90b4f1ca397300f7da50
Charlie Stephen Joseph Amos <csja3@itl155.student.eecs.qmul.ac.uk>
2018-03-13 12:42:23 +0000

    Test

---
+++ SEPRoject/User.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author csja3
+ */
+public class User {
+    int currency = 10;
+    String name;
+    String password;
+}
